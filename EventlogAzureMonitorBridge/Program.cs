a0b719a74d0c9da491b2e48cc0d49eb46ee8cc99
Manabu Tonosaki <manabu@tomarika.com>
2020-04-17 09:59:30 +0900

    Core Version Template

--- EventlogAzureMonitorBridge/Program.cs
+++ EventlogAzureMonitorBridge/Program.cs
@@ -1,68 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Configuration.Install;
-using System.Diagnostics.Eventing.Reader;
-using System.Globalization;
 using System.Linq;
-using System.Reflection;
-using System.ServiceProcess;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-namespace EventlogAzureMonitorBridge
+namespace EventLogAzureMonitorBridge
 {
-    static class Program
+    public class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-			args = Environment.GetCommandLineArgs();   // Necessary to get command args
-            var service = new Service1();
+            CreateHostBuilder(args).Build().Run();
+        }
 
-			if (Environment.UserInteractive)    // Console version
-			{
-				if (args.Length > 0)
-				{
-					// Support to install myself.
-					var isServiceExists = ServiceController.GetServices().Any(s => s.ServiceName == service.ServiceName);
-					var path = Assembly.GetExecutingAssembly().Location;
-					switch (args[0].ToLower())
-					{
-						case "/i":
-							if (isServiceExists)
-							{
-								Console.WriteLine($"The service named {service.ServiceName} have already installed.");
-							}
-							else
-							{
-								ManagedInstallerClass.InstallHelper(new[] { path });
-							}
-							return;
-						case "/u":
-							if (isServiceExists)
-							{
-								ManagedInstallerClass.InstallHelper(new[] { "/u", path });
-							}
-							else
-							{
-								Console.WriteLine($"The service '{service.ServiceName}' is not installed yet.");
-							}
-							return;
-					}
-				}
-				service.OnStartConsole(args);
-				service.OnStopConsole();
-
-				Console.WriteLine($"\r\n\r\n==== Hit any key to exit.");
-				Console.Read();
-			}
-			else
-			{    // Run from windows service
-				ServiceBase.Run(new[] { service, });
-			}
-		}
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                });
     }
 }
